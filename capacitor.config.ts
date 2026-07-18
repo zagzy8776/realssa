d348cbbdc48c0fee2370b0e350ef0d8585f409ad
zagzy8776 <amadiisdore92@gmail.com>
2026-07-18 05:52:40 +0200

    chore: bump version to 0.0.63 for Capgo OTA release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -29,7 +29,7 @@ const config: CapacitorConfig = {
     },
     CapacitorUpdater: {
       appId: 'com.realssa.news',
-      version: '0.0.61',
+      version: '0.0.63',
       autoUpdate: 'always',
       autoSplashscreen: true
     }
