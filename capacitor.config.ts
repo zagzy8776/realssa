4225a1e59b3d13f86efc35178b784d4088db84aa
zagzy8776 <amadiisdore92@gmail.com>
2026-07-27 18:02:11 +0200

    chore: bump to v0.0.77 + include android gradle files for Play Store build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -29,7 +29,7 @@ const config: CapacitorConfig = {
     },
     CapacitorUpdater: {
       appId: 'com.realssa.news',
-      version: '0.0.76',
+      version: '0.0.77',
       autoUpdate: 'always',
       autoSplashscreen: true
     }
