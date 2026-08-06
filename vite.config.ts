11e5801a495fedd163bb9244270c6bb9037d858b
zagzy8776 <amadiisdore92@gmail.com>
2026-08-06 23:30:53 +0200

    fix: change assets base path to root absolute / to solve MIME type errors on sub-routes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",
   server: {
+
     host: "::",
     port: 8080,
     hmr: {
